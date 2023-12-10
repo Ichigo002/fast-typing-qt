373591ec934d78fbfc670bd73c52c3ed0beaf5f9
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2023-12-10 16:10:33 +0100

    create gui

--- fast-typing/main.cpp
+++ fast-typing/main.cpp
@@ -3,10 +3,17 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // QSS Style File
+    QFile ssFile("./dark-theme.qss");
+    ssFile.open(QFile::ReadOnly);
+    QString style = QLatin1String(ssFile.readAll());
+    a.setStyleSheet(style);
 
     // Translation
     QTranslator translator;
